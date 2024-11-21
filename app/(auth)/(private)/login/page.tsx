03b4a726bcbdcf73570da5f0dbfa6de125911a7f
nausheenrizwankhan <nonikhanf1@gmail.com>
2024-11-22 00:08:09 +0500

    first comit

---
+++ app/(auth)/(private)/login/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <h2>Enter your name : </h2>
+      <input type="text" placeholder='Enter your name' />
+      <h2>Enter your Email :</h2>
+      <input type="email" placeholder='Enter your email' />
+    </div>
+  )
+}
+
+export default page
